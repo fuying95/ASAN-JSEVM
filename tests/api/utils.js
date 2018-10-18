cfec713642a394ef142d1b3c113450d81373b45d
fuying95 <dixx1995_xx@163.com>
2018-10-18 12:21:05 +0800

    10181220

---
+++ tests/api/utils.js
@@ -0,0 +1,37 @@
+const Block = require('ethereumjs-block')
+const Account = require('ethereumjs-account')
+const Level = require('levelup')
+const Blockchain = require('ethereumjs-blockchain')
+const VM = require('../../lib/index')
+
+function createGenesis () {
+  const genesis = new Block()
+  genesis.setGenesisParams()
+
+  return genesis
+}
+
+function createAccount () {
+  const raw = {
+    nonce: '0x00',
+    balance: '0xfff384'
+  }
+  const acc = new Account(raw)
+  return acc
+}
+
+function setupVM () {
+  const db = new Level('', {
+    db: require('memdown')
+  })
+  const blockchain = new Blockchain(db)
+  const vm = new VM({ blockchain })
+
+  return vm
+}
+
+module.exports = {
+  createGenesis,
+  createAccount,
+  setupVM
+}
